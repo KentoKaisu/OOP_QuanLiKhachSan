f8149b7859a7e2f9307ab971458b767be7f777d1
thanh vo <117738387+itachi61@users.noreply.github.com>
2024-12-01 17:59:44 +0700

    Update Staff.java
    edit 1/12

--- Staff.java
+++ Staff.java
@@ -1,4 +1,6 @@
+import java.util.Date;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 
 public class Staff {
         private int staffID;
@@ -7,24 +9,48 @@ public class Staff {
         private String lastName;
         private double salary;
         private String sex;
+        private Date birthDate;
+        private Date startDate;
 
-        public Staff(){}
-        public Staff(int staffID, String firstName, String midName, String lastName, double salary, String sex) {
+        private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+
+    public Staff(){}
+
+    public Staff(int staffID, String firstName, String midName, String lastName, double salary, String sex, Date birthDate, Date startDate) {
         this.staffID = staffID;
         this.firstName = firstName;
         this.midName = midName;
         this.lastName = lastName;
         this.salary = salary;
         setSex(sex);  // Sử dụng phương thức setSex để đảm bảo giới tính hợp lệ
+        this.birthDate = birthDate;
+        this.startDate = startDate;
     }
 
-        public void setSex(String sex) {
-            if (sex.equalsIgnoreCase("Nam") || sex.equalsIgnoreCase("Nữ") || sex.equalsIgnoreCase("Khác")) {
-                this.sex = sex;
-            } else {
-                throw new IllegalArgumentException("Giới tính phải là 'Nam', 'Nữ' hoặc 'Khác'.");
-            }
+    public void setSex(String sex) {
+        if (sex.equalsIgnoreCase("Nam") || sex.equalsIgnoreCase("Nữ") || sex.equalsIgnoreCase("Khác")) {
+                    this.sex = sex;
+        } else {
+                    throw new IllegalArgumentException("Giới tính phải là 'Nam', 'Nữ' hoặc 'Khác'.");
         }
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 
     public String getSex() {
         return sex;
@@ -84,8 +110,9 @@ public class Staff {
                 ", lastName='" + lastName + '\'' +
                 ", salary=" + df.format(salary) +
                 ", sex='" + sex + '\'' +
+                ", birthDate=" + dateFormat.format(birthDate) +
+                ", startDate=" + dateFormat.format(startDate) +
                 '}';
     }
-
-
 }
+
